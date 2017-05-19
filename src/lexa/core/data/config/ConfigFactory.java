8d0b2a44c5395570de40eea073721ee00e50e172
William Norman-Walker <william.norman.walker@gmail.com>
2017-05-19 18:50:05 +0100

    comment edits

--- src/lexa/core/data/config/ConfigFactory.java
+++ src/lexa/core/data/config/ConfigFactory.java
@@ -1,25 +1,13 @@
 /*
- * =============================================================================
+ *==============================================================================
  * Lexa - Property of William Norman-Walker
- * -----------------------------------------------------------------------------
+ *------------------------------------------------------------------------------
  * ConfigFactory.java
  *------------------------------------------------------------------------------
  * Author:  William Norman-Walker
  * Created: Month YEAR
- *------------------------------------------------------------------------------
- * Change Log
- * Date:        By: Description:
- * ----------   --- ------------------------------------------------------------
- * -            -   -
  *==============================================================================
  */
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package lexa.core.data.config;
 
 import lexa.core.data.BaseFactory;
