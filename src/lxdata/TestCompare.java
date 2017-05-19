218b5b3f3d9d837e3fe1d3ba13fe3f0f805c520d
William Norman-Walker <william.norman.walker@gmail.com>
2017-05-19 13:50:11 +0100

    Make the data objects comparable

---
+++ src/lxdata/TestCompare.java
@@ -0,0 +1,23 @@
+/*
+ * =============================================================================
+ * Lexa - Property of William Norman-Walker
+ * -----------------------------------------------------------------------------
+ * TestCompare.java
+ *------------------------------------------------------------------------------
+ * Author:  William Norman-Walker
+ * Created: Month YEAR
+ *==============================================================================
+ */
+package lxdata;
+
+import lexa.test.TestClass;
+
+/**
+ *
+ * @author william
+ */
+class TestCompare
+        extends TestClass {
+
+
+}
